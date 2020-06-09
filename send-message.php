55d843bd01af6b2eeac2b364f263adf3c3fe9099
tommcgreavy <66681937+tommcgreavy@users.noreply.github.com>
2020-06-09 14:29:19 +0100

    Add files via upload

---
+++ send-message.php
@@ -0,0 +1,20 @@
+<?php
+
+    if(isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $bizname = $_POST['bizname'];
+        $email = $_POST['email'];
+        $service = $_POST['service'];
+        $message = $_POST['message'];
+
+        $to = "info@tommcgreavy.co.uk";
+        $txt = "You have recieved a message from: ".$name."\n\nBusiness name: ".$bizname."\n\nThey are messaging you about: ".$service."\n\nMessage: ".$message."\n\nYou can contact them back on: ".$email;
+        $subject = "Website Message";
+        
+        $headers = "From: info@tommcgreavy.co.uk";
+
+        mail($to, $subject, $txt, $headers);
+        header("Location: index?sent=true#contactStrip");
+    }
+
+?>
